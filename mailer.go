97532bcae5bb0204f65a5f7e67d3f416b8f06dcb
Muhammad Ilham <muhd.iiilham@gmail.com>
2020-11-09 15:55:37 +0700

    ayolah

--- mailer.go
+++ mailer.go
@@ -10,17 +10,17 @@ import (
 
 func sendMail(email string, imams []string) {
 
-	body := "<strong> Alhamdulillah Sudara Kita Yang Beruntung Jadi Imam </strong> \n"
+	body := "<h3> Alhamdulillah Sudara Kita Yang Beruntung Jadi Imam </h3>"
 
 	for i, user := range imams {
-		body += fmt.Sprintf("%d. %s \n", i+1, user)
+		body += fmt.Sprintf("%d. %s <br>", i+1, user)
 	}
 
 	m := gomail.NewMessage()
 	m.SetHeader("From", "nonatheist@noreply.com")
 	m.SetHeader("To", email)
 	m.SetHeader("Subject", "Yang Beruntung Jadi Imam")
-	m.SetBody("text/plain", body)
+	m.SetBody("text/html", body)
 
 	d := gomail.NewDialer("smtp.gmail.com", 587, "slimyhebat@gmail.com", os.Getenv("EMAIL_PASSWORD"))
 
--- main.go
+++ main.go
@@ -1,50 +1,55 @@
 package main
 
 import (
-	"time"
-
-	"github.com/gin-gonic/gin"
-	"github.com/robfig/cron/v3"
+	"fmt"
 )
 
 var jamaah []*user
 
 func main() {
-	l, _ := time.LoadLocation("Asia/Jakarta")
-	now := time.Now()
-	r := gin.Default()
-	c := cron.New(cron.WithLocation(l))
+	// l, _ := time.LoadLocation("Asia/Jakarta")
+	// now := time.Now()
+	// r := gin.Default()
+	// c := cron.New(cron.WithLocation(l))
 
+	candidate := []string{}
 	jamaah = newJamaah()
+	randdd := randomIndex(len(jamaah))
+	for _, index := range randdd {
+		candidate = append(candidate, jamaah[index].Name)
+		fmt.Println(jamaah[index])
+	}
+	// sendMail("muhd.iiilham@gmail.com", candidate)
 
-	// Dzuhur
-	c.AddFunc("CRON_TZ=Asia/Jakarta 30 12 * * 1-5", getImams)
+	// 	// Dzuhur
+	// 	c.AddFunc("CRON_TZ=Asia/Jakarta 40 13 * * 1-5", getImams)
 
-	// Ashar
-	c.AddFunc("CRON_TZ=Asia/Jakarta 0 16 * * 1-5", getImams)
+	// 	// Ashar
+	// 	c.AddFunc("CRON_TZ=Asia/Jakarta 0 16 * * 1-5", getImams)
 
-	// Maghrib
-	c.AddFunc("CRON_TZ=Asia/Jakarta 0 4 * * 1-5", getImams)
+	// 	// Maghrib
+	// 	c.AddFunc("CRON_TZ=Asia/Jakarta 0 4 * * 1-5", getImams)
 
-	c.Start()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"currentTime": now,
-			"list":        jamaah,
-		})
-	})
-	r.Run(":8080")
+	// 	c.Start()
+	// 	r.GET("/ping", func(c *gin.Context) {
+	// 		c.JSON(200, gin.H{
+	// 			"currentTime": now,
+	// 			"list":        jamaah,
+	// 		})
+	// 	})
+	// 	r.Run(":8080")
 }
 
-func getImams() {
-	randomIndex := randomIndex(len(jamaah))
-	imams := []string{}
-	for i := range randomIndex {
-		imams = append(imams, jamaah[i].Name)
-	}
-	for _, user := range jamaah {
-		if user.Email != "" {
-			sendMail(user.Email, imams)
-		}
-	}
-}
+// func getImams() {
+// 	log.Println("Generating")
+// 	randomIndex := randomIndex(len(jamaah))
+// 	imams := []string{}
+// 	for i := range randomIndex {
+// 		imams = append(imams, jamaah[i].Name)
+// 	}
+// 	for _, user := range jamaah {
+// 		if user.Email != "" {
+// 			sendMail(user.Email, imams)
+// 		}
+// 	}
+// }
